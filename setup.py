c585fbdb660f4aa895b5242c760b9576f8719132
Hank Adler <henryaguila1988@gmail.com>
2022-09-04 17:20:45 -0400

    update version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='header',
-    version='0.1.0',
+    version='0.3.0',
     description='Parses module header',
     author='Hank Adler',
     packages=['header'],
